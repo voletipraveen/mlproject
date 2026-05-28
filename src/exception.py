dd20c72d496a90cceed96d0388e97cd87570ac21
voletipraveen <voletipraveen370@gmail.com>
2026-05-28 13:07:36 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,20 @@
+import sys
+import logging 
+
+def error_details_message(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="The Error has been occured in python script name [{0}] linenumber [{1}] and error is [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_details_message(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+
+
+ 
