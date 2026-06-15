da73e9670d1bd12f7a6ac37e2695df3d50952123
voletipraveen <voletipraveen370@gmail.com>
2026-06-16 02:37:57 +0530

    App Ready and HTML Screen

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 
 
@@ -48,6 +49,17 @@ def evaluate_model(X_train,y_train,X_test,y_test,models,param):
             return report
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
         
 
 
